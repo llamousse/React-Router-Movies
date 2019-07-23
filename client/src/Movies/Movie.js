7b704822a755a5ede3cd16c7b476d068df61f8c8
Vicky <vyue013@gmail.com>
2019-07-23 14:14:43 -0700

    Stretch: Added Saved Movies functionality and turned Saved Movie list into Links

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -23,10 +23,10 @@ const Movie = (props) => {
   }, [props.match.params.id]);
 
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie);
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -41,7 +41,7 @@ const Movie = (props) => {
         metascore={metascore}
         stars={stars}
       />
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={saveMovie}>Save</div>
     </div>
   );
 }
